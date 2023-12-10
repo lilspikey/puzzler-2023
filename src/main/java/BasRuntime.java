47af89809e6f05ac64b2d467069ecd145cc96f65
John Montgomery <john@littlespikeyland.com>
2023-12-10 13:54:32 +0000

    Integration test setup so we can verify generated byte code

--- src/main/java/BasRuntime.java
+++ src/main/java/BasRuntime.java
@@ -1,4 +1,5 @@
-import java.text.DecimalFormat;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.text.MessageFormat;
 import java.util.Scanner;
 
@@ -7,7 +8,10 @@ import java.util.Scanner;
  provides methods that are useful for implementing some functionality,
  without having to manually create lots of byte code
  */
-public class BasRuntime {
+public class BasRuntime implements Runnable {
+    // these are here so we can swap them out in tests
+    private InputStream in = System.in;
+    private PrintStream out = System.out;
     private int currentTab = 0;
 
     float fnINT(float f) {
@@ -19,7 +23,7 @@ public class BasRuntime {
     }
 
     void print(String s) {
-        System.out.print(s);
+        out.print(s);
         currentTab += s.length();
     }
 
@@ -28,20 +32,37 @@ public class BasRuntime {
     }
 
     void println() {
-        System.out.println();
+        out.println();
         currentTab = 0;
     }
 
     float inputFloat() {
-        var in = new Scanner(System.in);
+        var in = new Scanner(this.in);
         while (!in.hasNextFloat()) {
             in.next();
         }
         return in.nextFloat();
     }
 
-    void run() {
+    @Override
+    public void run() {
 
+    }
+
+    public InputStream getIn() {
+        return in;
+    }
+
+    public void setIn(InputStream in) {
+        this.in = in;
+    }
+
+    public PrintStream getOut() {
+        return out;
+    }
+
+    public void setOut(PrintStream out) {
+        this.out = out;
     }
 
     public static void main(String[] args) {
---
+++ src/test/java/IntegrationTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.beans.PropertyDescriptor;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IntegrationTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = { "hello" })
+    void givenSource_whenCompilingAndRunning_thenCorrectOutputGenerated(String exampleDir) throws Exception {
+        var javaAsm = new JavaASM();
+        var inputSource = "examples/" + exampleDir + "/input.bas";
+        try (var in = new BufferedInputStream(getClass().getResourceAsStream(inputSource))) {
+            var parser = new Parser();
+            var program = parser.parse(new InputStreamReader(in));
+            program.visit(javaAsm);
+        }
+        var className = exampleDir + "_test";
+        var bytes = javaAsm.generateClass(className);
+        var classLoader = new ClassLoader() {
+            @Override
+            protected Class<?> findClass(String name) {
+                return defineClass(name, bytes, 0, bytes.length);
+            }
+        };
+
+        Class<? extends Runnable> clazz = (Class) classLoader.findClass(className);
+        Runnable runnable = clazz.getDeclaredConstructor().newInstance();
+        PropertyDescriptor outProperty = new PropertyDescriptor("out", clazz);
+        ByteArrayOutputStream capturedOutput = new ByteArrayOutputStream();
+        try (PrintStream printOut = new PrintStream(capturedOutput)) {
+            outProperty.getWriteMethod().invoke(runnable, printOut);
+            runnable.run();
+        }
+
+        assertEquals(
+            readResource("examples/" + exampleDir + "/output.txt"),
+            capturedOutput.toString(StandardCharsets.UTF_8)
+        );
+    }
+
+    private String readResource(String path) throws URISyntaxException, IOException {
+        return Files.readString(Paths.get(getClass().getClassLoader().getResource(path).toURI()));
+    }
+
+}
