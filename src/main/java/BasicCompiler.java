79062be67cec8e31f0a8931792e611160d74485e
John Montgomery <john@littlespikeyland.com>
2023-12-22 17:59:47 +0000

    Add option to run compile code immediately
    So no need to separetly call java on the resulting class file

--- src/main/java/BasicCompiler.java
+++ src/main/java/BasicCompiler.java
@@ -2,26 +2,60 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Path;
 
 public class BasicCompiler {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
+        String sourceFile = null;
+        var run = false;
+        var list = false;
+        for (var arg: args) {
+            if (arg.startsWith("-")) {
+                if (arg.equals("--run")) {
+                    run = true;
+                } else if (arg.equals("--list")) {
+                    list = true;
+                } else {
+                    throw new IllegalArgumentException("Unknown argument: " + arg);
+                }
+            } else {
+                if (sourceFile != null) {
+                    throw new IllegalArgumentException("Unexpected argument: " + arg);
+                }
+                sourceFile = arg;
+            }
+        }
+        
         var javaAsm = new JavaASM();
-        var sourceFile = args[0];
         try (var in = new BufferedInputStream(new FileInputStream(sourceFile))) {
             var parser = new Parser();
             var program = parser.parse(new InputStreamReader(in));
-            program.visit(new ProgramListing());
+            if (list) {
+                program.visit(new ProgramListing());
+            }
             program.visit(javaAsm);
         }
         var className = new File(sourceFile).getName().replaceAll("[^a-z]", "_");
-        var classFileName = className + ".class";
         var bytes = javaAsm.generateClass(className);
-        try (var out = new FileOutputStream(classFileName)) {
-            out.write(bytes);
+        
+        if (run) {
+            var classLoader = new ClassLoader() {
+                @Override
+                protected Class<?> findClass(String name) {
+                    return defineClass(name, bytes, 0, bytes.length);
+                }
+            };
+
+            Class<? extends Runnable> clazz = (Class) classLoader.findClass(className);
+            Runnable runnable = clazz.getDeclaredConstructor().newInstance();
+            runnable.run();
+        } else {
+            var classFileName = className + ".class";
+            try (var out = new FileOutputStream(classFileName)) {
+                out.write(bytes);
+            }
+            System.out.println(classFileName);
         }
-        System.out.println(classFileName);
     }
 }
